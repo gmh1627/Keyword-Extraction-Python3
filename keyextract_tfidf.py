e4424ac16694bdfabc11ce7c0789b95655f00a8f
KangarooGao <3558965271@qq.com>
2023-12-22 15:35:23 +0800

    keyextract_tfidf.py

--- keyextract_tfidf.py
+++ keyextract_tfidf.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-# coding=utf-8
 # 采用TF-IDF方法提取文本关键词
 # http://scikit-learn.org/stable/modules/feature_extraction.html#tfidf-term-weighting
 import sys,codecs
@@ -18,6 +16,7 @@ from sklearn.feature_extraction.text import CountVectorizer
            4、对应的tfidf矩阵
 """
 # 数据预处理操作：分词，去停用词，词性筛选
+#@profile
 def dataPrepos(text, stopkey):
     l = []
     pos = ['n', 'nz', 'v', 'vd', 'vn', 'l', 'a', 'd']  # 定义选取的词性
@@ -28,6 +27,7 @@ def dataPrepos(text, stopkey):
     return l
 
 # tf-idf获取文本top10关键词
+#@profile
 def getKeywords_tfidf(data,stopkey,topK):
     idList, titleList, abstractList = data['id'], data['title'], data['abstract']
     corpus = [] # 将所有文档输出到一个list中，一行就是一个文档
@@ -36,7 +36,6 @@ def getKeywords_tfidf(data,stopkey,topK):
         text = dataPrepos(text,stopkey) # 文本预处理
         text = " ".join(text) # 连接成字符串，空格分隔
         corpus.append(text)
-
     # 1、构建词频矩阵，将文本中的词语转换成词频矩阵
     vectorizer = CountVectorizer()
     X = vectorizer.fit_transform(corpus) # 词频矩阵,a[i][j]:表示j词在第i个文本中的词频
@@ -44,18 +43,18 @@ def getKeywords_tfidf(data,stopkey,topK):
     transformer = TfidfTransformer()
     tfidf = transformer.fit_transform(X)
     # 3、获取词袋模型中的关键词
-    word = vectorizer.get_feature_names()
+    word = vectorizer.get_feature_names_out()
     # 4、获取tf-idf矩阵，a[i][j]表示j词在i篇文本中的tf-idf权重
     weight = tfidf.toarray()
     # 5、打印词语权重
     ids, titles, keys = [], [], []
     for i in range(len(weight)):
-        print u"-------这里输出第", i+1 , u"篇文本的词语tf-idf------"
+        print("-------这里输出第", i+1 , "篇文本的词语tf-idf------")
         ids.append(idList[i])
         titles.append(titleList[i])
         df_word,df_weight = [],[] # 当前文章的所有词汇列表、词汇对应权重列表
         for j in range(len(word)):
-            print word[j],weight[i][j]
+            print (word[j],weight[i][j])
             df_word.append(word[j])
             df_weight.append(weight[i][j])
         df_word = pd.DataFrame(df_word,columns=['word'])
@@ -65,18 +64,17 @@ def getKeywords_tfidf(data,stopkey,topK):
         keyword = np.array(word_weight['word']) # 选择词汇列并转成数组格式
         word_split = [keyword[x] for x in range(0,topK)] # 抽取前topK个词汇作为关键词
         word_split = " ".join(word_split)
-        keys.append(word_split.encode("utf-8"))
-
+        keys.append(word_split)
     result = pd.DataFrame({"id": ids, "title": titles, "key": keys},columns=['id','title','key'])
     return result
 
-
+#@profile
 def main():
     # 读取数据集
     dataFile = 'data/sample_data.csv'
     data = pd.read_csv(dataFile)
     # 停用词表
-    stopkey = [w.strip() for w in codecs.open('data/stopWord.txt', 'r').readlines()]
+    stopkey = [w.strip() for w in codecs.open('data/stopWord.txt', 'r', encoding='utf-8').readlines()]
     # tf-idf关键词抽取
     result = getKeywords_tfidf(data,stopkey,10)
     result.to_csv("result/keys_TFIDF.csv",index=False)
